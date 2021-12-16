4c2c9d83d4fca59189d288469ceffcb65a7c95e2
Moniaw090 <95939489+moniaw090@users.noreply.github.com>
2021-12-16 05:53:02 +0100

    Add files via upload
    Menu

---
+++ MenuAll/Scripts/Menu/Djalogmonia.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Djalogmonia : MonoBehaviour
+{
+  public void djalog()
+    {
+        SceneManager.LoadScene(3);
+    }
+}
